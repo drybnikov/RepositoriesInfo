8c86f39b38d6706e813b657ee6ec009770b649a5
Denis <drl1ght6@gmail.com>
2019-01-10 22:14:27 +0100

    Call repository request

--- app/src/main/java/com/test/denis/repositoriesinfo/ui/RepositoryListActivity.kt
+++ app/src/main/java/com/test/denis/repositoriesinfo/ui/RepositoryListActivity.kt
@@ -1,14 +1,45 @@
 package com.test.denis.repositoriesinfo.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.test.denis.repositoriesinfo.R
 import com.test.denis.repositoriesinfo.di.Injectable
+import com.test.denis.repositoriesinfo.network.RepoRepository
+import dagger.android.AndroidInjection
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import javax.inject.Inject
 
 class RepositoryListActivity : AppCompatActivity(), Injectable {
+
+    @Inject
+    lateinit var repository: RepoRepository
+
+    private lateinit var disposable: Disposable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_repository_list)
+
+        AndroidInjection.inject(this)
+
+        if (savedInstanceState == null)
+            loadData()
+    }
+
+    private fun loadData() {
+        disposable = repository
+            .searchRepo("kotlinbackend")
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { Log.d("loadData", "data: $it") },
+                { Log.e("loadData", "error: $it") }
+            )
+    }
+
+    override fun onDestroy() {
+        disposable.dispose()
+        super.onDestroy()
     }
 }
