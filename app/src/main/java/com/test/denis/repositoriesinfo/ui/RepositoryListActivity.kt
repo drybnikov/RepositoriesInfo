8c6dde232c8d11bbf5b4a7b3ea69c72a9143080a
Denis <drl1ght6@gmail.com>
2019-01-11 09:30:47 +0100

    Added recyclerview and adapter Added progress Style tuning

--- app/src/main/java/com/test/denis/repositoriesinfo/ui/RepositoryListActivity.kt
+++ app/src/main/java/com/test/denis/repositoriesinfo/ui/RepositoryListActivity.kt
@@ -2,21 +2,51 @@ package com.test.denis.repositoriesinfo.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.test.denis.repositoriesinfo.R
 import com.test.denis.repositoriesinfo.di.Injectable
+import com.test.denis.repositoriesinfo.model.Repo
+import kotlinx.android.synthetic.main.activity_repository_list.*
 import javax.inject.Inject
 
-class RepositoryListActivity : AppCompatActivity(), Injectable {
+class RepositoryListActivity : AppCompatActivity(), Injectable, RepositoryListView {
 
     @Inject
     lateinit var presenter: RepositoryListPresenter
+
+    private lateinit var viewAdapter: RepositoryListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_repository_list)
 
-        if (savedInstanceState == null)
-            presenter.onAttach()
+        initList()
+        presenter.onAttach(this)
+    }
+
+    private fun initList() {
+        viewAdapter = RepositoryListAdapter()
+        contentList.apply {
+            layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+            setHasFixedSize(true)
+            adapter = viewAdapter
+        }
+    }
+
+    override fun showRepositoryList(channels: List<Repo>) {
+        viewAdapter.apply {
+            items = channels
+            notifyDataSetChanged()
+        }
+    }
+
+    override fun setProgressVisibility(visible: Boolean) {
+        if (visible) {
+            progressBar.show()
+        } else {
+            progressBar.hide()
+        }
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/test/denis/repositoriesinfo/ui/RepositoryListPresenter.kt
+++ app/src/main/java/com/test/denis/repositoriesinfo/ui/RepositoryListPresenter.kt
@@ -1,6 +1,8 @@
 package com.test.denis.repositoriesinfo.ui
 
 import android.util.Log
+import com.test.denis.repositoriesinfo.model.Repo
+import com.test.denis.repositoriesinfo.model.RepositoryResponse
 import com.test.denis.repositoriesinfo.network.RepoRepository
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
@@ -8,18 +10,35 @@ import javax.inject.Inject
 
 class RepositoryListPresenter @Inject constructor(private val repository: RepoRepository) {
     private lateinit var disposable: Disposable
+    private var view: RepositoryListView? = null
+    private var currentPage: Int = 1
 
-    fun onAttach() {
+    fun onAttach(view: RepositoryListView) {
+        this.view = view
+
         disposable = repository
-            .searchRepo("kotlinbackend")
+            .searchRepo(query = "tetris", page = currentPage)
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(
-                { Log.d("loadData", "data: $it") },
-                { Log.e("loadData", "error: $it") }
-            )
+            .doOnSubscribe { this.view?.setProgressVisibility(true) }
+            .subscribe(this::onDataLoaded) { Log.e("loadData", "error: $it") }
+    }
+
+    private fun onDataLoaded(data: RepositoryResponse) {
+        Log.d("loadData", "data: $data")
+
+        view?.apply {
+            showRepositoryList(data.items.sortedBy { it.size })
+            setProgressVisibility(false)
+        }
     }
 
     fun onDetach() {
+        view = null
         disposable.dispose()
     }
+}
+
+interface RepositoryListView {
+    fun showRepositoryList(repos: List<Repo>)
+    fun setProgressVisibility(visible: Boolean)
 }
